0af91d502073b770ed3a8ef710f0daa5b1bce053
codegenerator-tools <14329682+codegenerator-tools@users.noreply.github.com>
2020-06-17 13:08:08 +0530

    Update AppRoomDatabase.kt

--- app/src/main/java/com/example/database/AppRoomDatabase.kt
+++ app/src/main/java/com/example/database/AppRoomDatabase.kt
@@ -10,11 +10,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-/**
- * This is the backend. The database. This used to be done by the OpenHelper.
- * The fact that this has very few comments emphasizes its coolness.  In a real
- * app, consider exporting the schema to help you with migrations.
- */
 @Database(entities = [Account::class, Artist::class], version = 1, exportSchema = false)
 //@TypeConverters(Converters::class)
 
@@ -32,9 +27,8 @@ companion object {
                 context: Context,
                 scope: CoroutineScope
         ): AppRoomDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
-            return INSTANCE ?: synchronized(this) {
+
+                return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                         context.applicationContext,
                         AppRoomDatabase::class.java,
